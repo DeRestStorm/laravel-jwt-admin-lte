9a41c4caa5ab383c79ac8fb10355dc1979146c42
dereststorm <deidara0909@hotmail.com>
2019-07-30 02:09:59 +0300

    init

---
+++ resources/js/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="wrapper">
+        <header-component></header-component>
+
+
+        <div class="content-wrapper">
+            <router-view></router-view>
+        </div>
+
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+            <div class="p-3">
+                <h5>Title</h5>
+                <p>Sidebar content</p>
+            </div>
+        </aside>
+        <!-- /.control-sidebar -->
+        <footer-component></footer-component>
+    </div>
+</template>
+<script>
+    import HeaderComponent from "./components/HeaderComponent";
+    import FooterComponent from "./components/FooterComponent";
+
+    export default {
+        components: {
+            HeaderComponent,
+            FooterComponent
+        }
+    }
+</script>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html style="height: auto;">
+<head>
+    <meta charset="utf-8">
+    <title>vue-admin-lte</title>
+    <meta content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no" name="viewport">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body class="hold-transition sidebar-mini" style="height: auto;">
+<div id="app">
+
+</div>
+<script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
